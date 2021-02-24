b66d16bfc099111dad6fe7067e0620aeef59a578
Roman Solovyov <solovjov.roman@gmail.com>
2021-02-24 19:46:06 +0300

    Posts editing

--- lib/pxblog_web/controllers/post_controller.ex
+++ lib/pxblog_web/controllers/post_controller.ex
@@ -1,62 +1,72 @@
 defmodule PxblogWeb.PostController do
   use PxblogWeb, :controller
   plug :assign_user
+  #import Ecto.Query, warn: false
+  import Ecto
+  alias Pxblog.Repo
 
   alias Pxblog.Posts
   alias Pxblog.Posts.Post
   alias Pxblog.Users.User
 
   def index(conn, _params) do
-    posts = Posts.list_posts()
+    posts = Repo.all(assoc(conn.assigns[:user], :posts))
     render(conn, "index.html", posts: posts)
   end
 
   def new(conn, _params) do
-    changeset = Posts.change_post(%Post{})
+    changeset =
+      conn.assigns[:user]
+      |> build_assoc(:posts)
+      |> Post.changeset(%{})
+
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"post" => post_params}) do
-    case Posts.create_post(post_params) do
-      {:ok, post} ->
+    changeset =
+      conn.assigns[:user]
+      |> build_assoc(:posts)
+      |> Post.changeset(post_params)
+    case Repo.insert(changeset) do
+      {:ok, _post} ->
         conn
         |> put_flash(:info, "Post created successfully.")
         |> redirect(to: Routes.user_post_path(conn, :index, conn.assigns[:user]))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
+      {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
   end
 
   def show(conn, %{"id" => id}) do
-    post = Posts.get_post!(id)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
     render(conn, "show.html", post: post)
   end
 
   def edit(conn, %{"id" => id}) do
-    post = Posts.get_post!(id)
-    changeset = Posts.change_post(post)
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
+    changeset = Post.changeset(post, %{})
     render(conn, "edit.html", post: post, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Posts.get_post!(id)
-
-    case Posts.update_post(post, post_params) do
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
+    changeset = Post.changeset(post, post_params)
+    case Repo.update(changeset) do
       {:ok, post} ->
         conn
         |> put_flash(:info, "Post updated successfully.")
         |> redirect(to: Routes.user_post_path(conn, :show, conn.assigns[:user], post))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
+      {:error, changeset} ->
         render(conn, "edit.html", post: post, changeset: changeset)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    post = Posts.get_post!(id)
-    {:ok, _post} = Posts.delete_post(post)
-
+    post = Repo.get!(assoc(conn.assigns[:user], :posts), id)
+    # Здесь мы используем delete! (с восклицательным знаком), потому что мы ожидаем
+    # что оно всегда будет работать (иначе возникнет ошибка).
+    Repo.delete!(post)
     conn
     |> put_flash(:info, "Post deleted successfully.")
     |> redirect(to: Routes.user_post_path(conn, :index, conn.assigns[:user]))
@@ -70,7 +80,14 @@ defmodule PxblogWeb.PostController do
         user = Repo.get(User, user_id)
         assign(conn, :user, user)
       _ ->
-        conn
+        invalid_user(conn)
     end
+  end
+
+  defp invalid_user(conn) do
+    conn
+    |> put_flash(:error, "Invalid user!")
+    |> redirect(to: Routes.page_path(conn, :index))
+    |> halt
   end
 end
---
+++ push.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -z $1 ]; then
+	echo "no commit name"
+	exit
+fi
+
+echo "commit with name: $1"
+
+git add *
+git commit -m "$1"
+git push origin master
